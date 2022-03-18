7f694f3210d3b82345761c0381774b0321160d89
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-19 00:50:57 +0600

    Take input from command line argument and print

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,6 +1,10 @@
 use std::env::{args, Args};
 
 fn main() {
-    let args: Args = args();
-    print!("Your command line input is {:?}", args);
+    let mut args: Args = args();
+    let first_num = args.nth(1).unwrap();
+    // since args contains iterator we want to access the next available elemen
+    let operator = args.nth(0).unwrap();
+    let second_num = args.nth(0).unwrap();
+    print!("{} {} {} = {}", first_num, operator, second_num, 420);
 }
