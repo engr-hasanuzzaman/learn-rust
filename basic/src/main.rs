95fb338d565a3dad91c7a68de8f75a3ddabb7bad
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-19 23:49:38 +0600

    String module keeping string related practices

--- basic/src/main.rs
+++ basic/src/main.rs
@@ -1,9 +1,11 @@
 // mod print;
 // mod vars;
-mod types;
+// mod types;
+mod string;
 
 fn main() {
     // print::run();
     // vars::run();
-    types::run();
+    // types::run();
+    string::run();
 }
---
+++ basic/src/string.rs
@@ -0,0 +1,7 @@
+// two type of strings, immutable and mutable
+
+pub fn run() {
+    let hello = "Hello World";
+
+    println!("{:?}", hello);
+}
