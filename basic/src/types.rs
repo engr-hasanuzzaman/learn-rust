6ca98acc15f16f9a87342678a1d581fb97980970
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-19 23:46:59 +0600

    Print emoji unicode char

--- basic/src/types.rs
+++ basic/src/types.rs
@@ -3,5 +3,6 @@
 // i8, i32......., f32, f64, Array, tuple, char, Boolean
 // rust flow snake case for variable naming
 pub fn run() {
-    println!("{:?}", (std::i32::MAX, std::f64::MAX));
+    let unicode_char = "\u{1F600}";
+    println!("{:?}", (std::i32::MAX, std::f64::MAX, unicode_char));
 }
