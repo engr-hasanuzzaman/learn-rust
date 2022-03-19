cac10828672f0e11de196f73c18319034f2b7045
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-19 23:59:34 +0600

    Practive both mutable & immutable string

--- basic/src/string.rs
+++ basic/src/string.rs
@@ -1,7 +1,10 @@
-// two type of strings, immutable and mutable
+// two type of strings, immutable and mutable heap allocated grawable datastructure
 
 pub fn run() {
-    let hello = "Hello World";
-
-    println!("{:?}", hello);
+    let hello = "Hello World"; // immutable string
+    let mut expandable_string = String::from("This is the beginning");
+    // two ways to add additional on top of expandable string
+    expandable_string.push_str(" with ending ");
+    expandable_string.push('\u{1F600}');
+    println!("{:?}", (hello, expandable_string));
 }
