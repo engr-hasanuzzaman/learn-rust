37897356dfc7a21f3e1c917373a5c1a76f326651
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-19 23:43:40 +0600

    Add convention comment

--- basic/src/types.rs
+++ basic/src/types.rs
@@ -1,7 +1,7 @@
 // statically type programming language but we can use implicit type based on the
 // initial value 
 // i8, i32......., f32, f64, Array, tuple, char, Boolean
-
+// rust flow snake case for variable naming
 pub fn run() {
     println!("{:?}", (std::i32::MAX, std::f64::MAX));
 }
