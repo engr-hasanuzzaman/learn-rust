45a056fed379fefb4dc6934a21f7d3849724cd30
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-19 23:42:55 +0600

    Module for types

---
+++ basic/src/types.rs
@@ -0,0 +1,7 @@
+// statically type programming language but we can use implicit type based on the
+// initial value 
+// i8, i32......., f32, f64, Array, tuple, char, Boolean
+
+pub fn run() {
+    println!("{:?}", (std::i32::MAX, std::f64::MAX));
+}
