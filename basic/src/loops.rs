9171981d87c5354627f8aacaf88c5421393898be
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-20 17:00:50 +0600

    Fixbuzz with while loop

--- basic/src/loops.rs
+++ basic/src/loops.rs
@@ -1,12 +1,27 @@
 pub fn run() {
     // intifinte loop
-    let mut count = 0;
+    let mut count = 1;
 
-    loop {
-        count += 1;
-        println!("The value {}", count);
-        if count == 20 {
-            break;
+    // loop {
+    //     count += 1;
+    //     println!("The value {}", count);
+    //     if count == 20 {
+    //         break;
+    //     }
+    // }
+
+    // while loop
+    while count  <= 100 {
+        if count % 15 == 0 {
+            println!("FizBuzz");
+        } else if count % 3 == 0 {
+            println!("Fiz");
+        } else if count % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", count)
         }
+
+        count += 1;
     }
 }
