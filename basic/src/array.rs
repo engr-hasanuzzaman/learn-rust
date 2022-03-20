3873c758792e74be97278deeba1d9a3732d42ce2
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-20 15:55:32 +0600

    Practive array operations

---
+++ basic/src/array.rs
@@ -0,0 +1,10 @@
+pub fn run() {
+    let mut a: [i32; 5] = [1,2,3,4,5];
+    println!("Full array is {:?}", a);
+    println!("First array element is {:?}", a[0]);
+
+    a[4] = 120;
+    println!("After chanig the full array is {:?}", a);
+    println!("array size is {:?}", a.len());
+    println!("array memo size is {:?}",std::mem::size_of_val(&a));
+}
--- basic/src/main.rs
+++ basic/src/main.rs
@@ -2,12 +2,14 @@
 // mod vars;
 // mod types;
 // mod string;
-mod tuple;
+// mod tuple;
+mod array;
 
 fn main() {
     // print::run();
     // vars::run();
     // types::run();
     // string::run();
-    tuple::run();
+    // tuple::run();
+    array::run();
 }
