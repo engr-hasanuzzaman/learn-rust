ce37ab3b7429b24c38bab355e8d7546515e7153e
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2022-03-22 22:07:50 +0600

    Create method for struct

--- basic/src/structs.rs
+++ basic/src/structs.rs
@@ -8,8 +8,16 @@ impl Person {
     fn new(name: &str, age: i32, gender: &str) -> Person{
         Person {
             name: name.to_string(),
-            age: age,
-            gender: gender.to_string()
+            age,
+            gender: gender.to_string(),
+        }
+    }
+
+    fn category(&self) -> &str {
+        if self.age > 10 {
+            "Matured"
+        } else {
+            "Teen"
         }
     }
 }
@@ -19,4 +27,5 @@ pub fn run() {
     let p = Person::new("Mac", 32, "male");
 
     println!("The struct is {}, age {}, gender: {}", p.name, p.age, p.gender);
+    println!("Persons category is {}", p.category());
 }
